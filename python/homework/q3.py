955f7ba2a2eda9526436ce5809649a4cead9049f
Xenos <xenosoden@gmail.com>
2023-10-15 19:43:38 +0800

    initial commit

--- python/homework/q3.py
+++ python/homework/q3.py
@@ -1,9 +1,13 @@
 num = input('Enter a number')  # let user set a number
 num = eval(num)                # change the data type of "num"
 
-little = 1                     # let the loop count start from 1
-while little ** 2 <= num:      # compare the little square with num
-    little = little + 1        # add 1 to the little and reset
+if num >= 1:                   # check if num is valid
+    little = 1                     # let the loop count start from 1
+    while little ** 2 <= num:      # compare the little square with num
+        little = little + 1        # add 1 to the little and reset
 
-print(little)                  # print the final answer
+    print(little)                  # print the final answer
+
+else:
+    print('invalid input')
     
