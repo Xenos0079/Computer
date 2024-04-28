a34ac3be84362524f947823d03ff4bdcd0b3ecd7
Xenos <xenosoden@gmail.com>
2024-04-28 23:22:10 +0800

    about snake

--- python/1002_HW/A3/snake_fin.py
+++ python/1002_HW/A3/snake_fin.py
@@ -60,25 +60,25 @@ def configure_play_area():
     '''
     # drawing the motion_border of the play area and motion_border of states_area at the top
     # mb / st / s / w / h = motion_border / states_area / size / weight / height
-    mb , st = create_turtle(0 , 0 , "" , "black")  , create_turtle(0 , 0 , "" , "black")
-    mb_s , st_w , st_h = DIM_PLAY_AREA // SZ_SQUARE  , DIM_STAT_AREA // SZ_SQUARE  , DIM_PLAY_AREA // SZ_SQUARE
+    mb , st = create_turtle(0 , 0 , "" , "black") , create_turtle(0 , 0 , "" , "black")
+    mb_s , st_w , st_h = DIM_PLAY_AREA // SZ_SQUARE , DIM_STAT_AREA // SZ_SQUARE , DIM_PLAY_AREA // SZ_SQUARE
     mb.shapesize(mb_s , mb_s , 3)
     st.shapesize(st_w , st_h , 3)
     mb.goto(0 , 0) # shift down half the status
-    st.goto(0 , DIM_PLAY_AREA // 2 + DIM_STAT_AREA // 2)  # shift up half the motion
+    st.goto(0 , DIM_PLAY_AREA // 2 + DIM_STAT_AREA // 2) # shift up half the motion
 
     # create a turtle to write introduction and another turtle to write status
     # it_w stands for intro_writer and st stands for status and w stands for writer
-    it_w , st_w = create_turtle(-200 , 0)  , create_turtle(0,0)
+    it_w , st_w = create_turtle(-160 , 115) , create_turtle(0,0)
     it_w.hideturtle()
     st_w.hideturtle()
-    it_w.write("Click anywhere to start the game\n\n" + "Use arrow keys to control the snake.\n\n" ,  \
+    it_w.write("Click anywhere to start the game\n\n" + "Use arrow keys to control the snake.\n\n" , \
                 font = FONT_INTRO)
     st_w.goto(0 , st.ycor() - DIM_STAT_AREA // 3)
 
     return it_w , st_w
 
-def configure_screen():
+def c_s(): # configure_screen
     '''
     Configure the screen
     '''
@@ -86,7 +86,7 @@ def configure_screen():
     sc = turtle.Screen() # initialize the screen
     sc.tracer(0) # disable auto screen refresher , [ 0 = disable , 1 = enable ]
     sc.title("Snake by TianCheng")
-    sc_w , sc_h = DIM_PLAY_AREA + DIM_MARGIN * 2  , DIM_PLAY_AREA + DIM_MARGIN * 2 + DIM_STAT_AREA * 2  , 
+    sc_w , sc_h = DIM_PLAY_AREA + DIM_MARGIN * 2 , DIM_PLAY_AREA + DIM_MARGIN * 2 + DIM_STAT_AREA * 2 , 
     sc.setup(sc_w , sc_h)
     sc.mode("standard") # create a standard screen
 
@@ -105,15 +105,12 @@ def update_status():
     g_status.write(st , font = FONT_STATUS , align = "center")
     g_screen.update()
 
-def on_arrow_key_pressed(key):
+def pressed(key):
     '''
     Event handler for arrow key press events.
     '''
-    global g_key_pressed  , g_pressed_history
+    global g_key_pressed
     g_key_pressed = key
-    if len(g_pressed_history) > 1:
-        g_pressed_history.pop(0)
-    g_pressed_history.append(key)
     update_status()
 
 def out_of_bound(x , y):
@@ -129,9 +126,9 @@ def random_position():
     x = random.randint(-DIM_PLAY_AREA // 2 + 15 , DIM_PLAY_AREA // 2 - 15)
     y = random.randint(-DIM_PLAY_AREA // 2 + 15 , DIM_PLAY_AREA // 2 - 15)
     x  , y = x - x % SZ_SQUARE  , y - y % SZ_SQUARE
-    if out_of_bound(x , y) or [x  , y] in g_postion_set or  (x ** 2 + y ** 2) < 100:
+    if out_of_bound(x , y) or [x  , y] in g_postion_set or (x ** 2 + y ** 2) * 0.5 < 100:
         return random_position()
-    g_postion_set.append([x  , y])
+    g_postion_set.append([x , y])
     return (x , y)
 
 def skip():
@@ -143,23 +140,24 @@ def skip():
         return True
     return False
 
-def on_timer_snake():
+def o_t_s(): # o_t_s
     '''
     Advances the snake's movement on a timer.
     '''
     if skip():
         return
-    global g_head_positions
-    if g_key_pressed is None or g_key_pressed == KEY_SPACE: # snake should standing still
+    global g_head_positions, g_press_history
+    if g_key_pressed is None \
+        or (g_key_pressed == KEY_SPACE and g_press_history[-2] == KEY_SPACE): # should be still
         check_collision()
         update_status()
         g_screen.update()
-        g_screen.ontimer(on_timer_snake , TIMER_SNAKE)
+        g_screen.ontimer(o_t_s , TIMER_SNAKE)
         return
     # snake is moving
     g_head.color(*COLOR_BODY) # 1/ change color (old head -> body_color)
     g_head.stamp() # 2/ ready to clone
-    g_head.color(COLOR_HEAD) # 3/ (change the old head color to head_color)
+    g_head.color(COLOR_HEAD) # 3/ (change the old head color to head_color
     g_head.setheading(HEADING_BY_KEY[g_key_pressed]) # 4/ get the position
     g_head.forward(SZ_SQUARE) # 5/ move the head
     new_head_pos = g_head.position() # 6/ get new position of the head
@@ -170,7 +168,7 @@ def on_timer_snake():
     check_collision()
     update_status() # 1/
     g_screen.update() # 2/
-    g_screen.ontimer(on_timer_snake , TIMER_SNAKE) # 3/ 
+    g_screen.ontimer(o_t_s , TIMER_SNAKE) # 3/ 
     # 1,2 and 3 cannot use chaotic since the snake must move instanly when the keypress occurs
 
 def chaotic_case(t_obj , ott):
@@ -182,7 +180,7 @@ def chaotic_case(t_obj , ott):
     delay = random.randint(t_obj - 15 , t_obj + 15)
     g_screen.ontimer(ott , delay) 
 
-def on_timer_monster():
+def o_t_m(): # on_timer_monster
     '''
     Advances the monster's movement based on the timer
     '''
@@ -190,7 +188,7 @@ def on_timer_monster():
         return
     global count
     if g_key_pressed is None:
-        g_screen.ontimer(on_timer_monster , TIMER_SNAKE)
+        g_screen.ontimer(o_t_m , TIMER_SNAKE)
         return
     for monster in g_monsters:
         ang = monster.towards(g_head) # get the relative position
@@ -198,17 +196,17 @@ def on_timer_monster():
         ori = qtr * 90 # ori = orientation
 
         monster.setheading(ori) # set the heading orientation
-        monster.forward(random.randint(SZ_SQUARE , SZ_SQUARE * 2)) # set the moving distance
-    chaotic_case(TIMER_MONSTER , on_timer_monster)
+        monster.forward(random.randint(SZ_SQUARE * 2 , SZ_SQUARE * 3)) # set the moving distance
+    chaotic_case(TIMER_MONSTER , o_t_m)
 
-def on_timer_food():
+def o_t_f(): # on_timer_food
     '''
     Advances the food items on the screen on a timer.
     '''
     if skip():
         return
     if g_key_pressed is None: # no input
-        g_screen.ontimer(on_timer_food , TIMER_SNAKE)
+        g_screen.ontimer(o_t_f , TIMER_SNAKE)
         return
     for food in g_foods:
         food , i = food # decomposite the food , give it a label
@@ -221,10 +219,10 @@ def on_timer_food():
         food.clear() # 2/ clear the previous food
         food.penup() # 3/ disable the pen
         # below are the integer writing process , the data int 14 is from a discussion.
-        food.sety(food.ycor() - 14)  # 1/ move the position to let the integer could be written in the center
+        food.sety(food.ycor() - 14) # 1/ move the position to let the integer could be written in the center
         food.write(str(i) , align = "center" , font = FONT_FOOD) # 2/ write the food
-        food.sety(food.ycor() + 14)  # 3/ move the real postition back to the initial position
-    chaotic_case(TIMER_FOOD  , on_timer_food)
+        food.sety(food.ycor() + 14) # 3/ move the real postition back to the initial position
+    chaotic_case(TIMER_FOOD , o_t_f)
 
 def check_collision():
     '''
@@ -232,21 +230,17 @@ def check_collision():
     '''
     global count , g_key_pressed , g_head_positions , g_tail, win
     for monster in g_monsters:
-        #head-monster collision
-        if g_head.distance(monster) < SZ_SQUARE:
+        if g_head.distance(monster) < SZ_SQUARE: # head / monster
             endgame()
-        #body-monster collision
-        if any(monster.distance(pos)<SZ_SQUARE for pos in g_head_positions):
+        if any(monster.distance(pos) < SZ_SQUARE for pos in g_head_positions): # body / monster
             count += 1
             update_status()
-    #head-edge collision
-    cor_edge = DIM_PLAY_AREA//2 - SZ_SQUARE
-    if abs(g_head.xcor()) >= cor_edge or abs(g_head.ycor()) >= cor_edge:
+    cor_edge = DIM_PLAY_AREA // 2 - SZ_SQUARE
+    if abs(g_head.xcor()) >= cor_edge or abs(g_head.ycor()) >= cor_edge: # head / edge
         g_key_pressed = KEY_SPACE
         update_status()
-    #head-food collision
     for food , i in g_foods:
-        if g_head.distance(food) < SZ_SQUARE:
+        if g_head.distance(food) < SZ_SQUARE: # head / food
             g_foods.remove((food , i))
             g_tail += i
             if len(g_foods) == 0:
@@ -258,7 +252,7 @@ def check_collision():
 
 def endgame():
     '''
-    Stops the game and displays the  message
+    Stops the game and displays the message
     '''
     global to_stop, win
     to_stop = True
@@ -274,9 +268,9 @@ def endgame():
     g_screen.update()
 
 
-def cb_start_game(x ,y):
+def cb_s(x ,y): # configure_start_game
     '''
-    Starts the game by setting up the initial game state and event handlers.  
+    Starts the game by setting up the initial game state and event handlers. 
     This function is called when the user clicks on the screen to start the game.
     '''
     global start_time
@@ -285,13 +279,14 @@ def cb_start_game(x ,y):
     g_intro.clear()
 
     for key in [KEY_UP , KEY_DOWN , KEY_LEFT , KEY_RIGHT , KEY_SPACE]:
-        g_screen.onkey(partial(on_arrow_key_pressed,key) , key)
+        g_screen.onkey(partial(pressed,key) , key)
 
-    on_timer_snake()
-    on_timer_monster()
-    on_timer_food()
+    o_t_s() # 1 /
+    o_t_m() # 2 /
+    o_t_f() # 3 /
+    # 1/2/3 set the timer for the objects 
 
-def create_food():
+def c_f(): # create_food
     '''
     Creates the specified number of food items on the screen.
     '''
@@ -304,9 +299,12 @@ def create_food():
         g_foods.append(food)
     return g_foods
 
-def create_monster():
+def c_m(): # create_monster
+    '''
+    Creates the monsters on the screen.
+    '''
     g_monsters = []
-    for _ in range(NUM_MONSTER):
+    for _ in range(1 , NUM_MONSTER + 1):
         x , y = random_position()
         monster = create_turtle(x , y , COLOR_MONSTER , "black")
         g_monsters.append(monster)
@@ -318,15 +316,13 @@ def main():
     '''
     global start_time , g_screen , g_intro , g_status , g_monsters , g_head , g_foods
     start_time = time.time()
-    g_screen = configure_screen()
+    g_screen = c_s()
     g_intro , g_status = configure_play_area()
-
     update_status()
-
     g_head = create_turtle(0 , 0 , COLOR_HEAD , "black")
-    g_monsters = create_monster()
-    g_foods = create_food()
-    g_screen.onscreenclick(cb_start_game) # set up a mouse-click call back
+    g_monsters = c_m()
+    g_foods = c_f()
+    g_screen.onscreenclick(cb_s) # set up a mouse-click event
     g_screen.update()
     g_screen.listen()
     g_screen.mainloop()
