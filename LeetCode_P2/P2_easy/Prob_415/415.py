2518e3f7ad2b8bb9db3c38bef0361f8d20fd4bab
Xenos <xenosoden@gmail.com>
2024-05-01 22:41:21 +0800

    441 arranging coins

--- LeetCode_P2/P2_easy/Prob_415/415.py
+++ LeetCode_P2/P2_easy/Prob_415/415.py
@@ -1,6 +1,8 @@
 # two number sum
 # need to know that the input are string
 
+import sys
+
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
         sys.set_int_max_str_digits(10000) ###
