60f1a5707f5a1770e466933ada9fd19cb9fbcbdc
Xenos <xenosoden@gmail.com>
2024-05-19 23:14:02 +0800

    3068 too hard

--- LCD_P3/D0011/0011c.py
+++ LCD_P3/D0011/0011c.py
@@ -9,18 +9,17 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def distributeCoins(self, root: Optional[TreeNode], parent = None) -> int:
-
+    def distributeCoins(self, root: Optional[TreeNode]) -> int:
+        
         self.moves = 0  # Initialize the total number of moves
 
         # Depth-first search to calculate the number of moves
         def dfs(node):
             if not node:
                 return 0
-            left_excess = dfs(node.left)  # Calculate excess coins in the left subtree
-            right_excess = dfs(node.right)  # Calculate excess coins in the right subtree
-            self.moves += abs(left_excess) + abs(right_excess)  # Update the total number of moves
-            return node.val + left_excess + right_excess - 1  # Return the excess coins at the current node
+            l, r = dfs(node.left), dfs(node.right)  # Calculate excess coins in the subtree
+            self.moves += abs(l) + abs(r)  # Update the total number of moves
+            return node.val + l + r - 1  # Return the excess coins at the current node
 
         dfs(root)  # Start the depth-first search from the root
         return self.moves  # Return the total number of moves
