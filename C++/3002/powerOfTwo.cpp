05d4c8d6d835142526675962ea940e8393eab1f0
Xenos <xenosoden@gmail.com>
2024-09-08 23:52:47 +0800

    modified

--- C++/3002/powerOfTwo.cpp
+++ C++/3002/powerOfTwo.cpp
@@ -10,8 +10,13 @@ using namespace std;
 
 /* Function prototypes */
 
-int raiseToPower(int n, int k);
-
+int raiseToPower(int n, int k) {
+   int result = 1;
+   for (int i = 0; i < k; i++) {
+      result *= n;
+   }
+   return result;
+}
 /* Main program */
 
 int main() {
